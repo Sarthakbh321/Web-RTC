6f26881b513437ca4543dc9b86ef7e175912eb33
Sarthak <sarthakbh321@gmail.com>
2020-08-03 00:04:11 +0530

    initialized local stream

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Redirect } from "react-router";
 import AgoraRTC from "agora-rtc-sdk";
+import './Chat.css';
+import { concatSeries } from "async";
 
 function Chat(props) {
 	const [redirect, setRedirect] = useState(false);
@@ -23,14 +25,13 @@ function Chat(props) {
 	}
 
 	useEffect(() => {
-		console.log(props.location.state);
 		if(props.location.state === undefined) {
 			setRedirect(true);
 			return;
 		}
 
 		setParams(props.location.state);
-		options.channel = props.location.state.room;
+		options.channel = props.location.state.params.room;
 	}, [props.location.state]);
 
 	useEffect(() => {
@@ -38,16 +39,50 @@ function Chat(props) {
 
 		rtc.client.init(options.appId, () => {
 			console.log("init success");
+
+			rtc.client.join(null, options.channel, null, (uid) => {
+				console.log(`joined channel ${options.channel} with uid: ${uid}`);
+				rtc.local = AgoraRTC.createStream({streamID: uid, audio: true, video: true, screen: false});
+
+				rtc.local.init(() => {
+					console.log("local stream initialized");
+					rtc.local.play("player");
+
+					rtc.client.publish(rtc.local, (error) => {
+						console.log("localstream publish failed ", error)
+					})
+				}, (error) => {
+					console.log("local stream init failed: ", error);
+				})
+			}, (error) => {
+				console.log("channel join failed: ", error);
+			})
+
 		}, (error) => {
-			console.log(error);
+			console.log("client init failed ", error);
 		})
+
+
+		return () => {
+			rtc.client.leave(() => {
+				rtc.local.stop();
+				rtc.local.close();
+				
+				console.log("client leave successful");
+			}, (error) => {
+				console.log("client leave failed");
+			})
+		}
 	}, [])
 
 	if(redirect) {
 		return <Redirect to="/" />
 	}
 	return (
-		<h1>HI</h1>
+		<>
+			<h1>HI</h1>
+			<div id="player"></div>
+		</>
 	)
 }
 
