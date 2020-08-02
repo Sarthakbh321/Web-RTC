f88af9b4702d19a8daf7c4a21ba35b28c801c073
Sarthak <sarthakbh321@gmail.com>
2020-08-02 23:16:41 +0530

    started basic agora implementation

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,6 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Redirect } from "react-router";
+import AgoraRTC from "agora-rtc-sdk";
 
-function Chat() {
+function Chat(props) {
+	const [redirect, setRedirect] = useState(false);
+	const [params, setParams] = useState({});
+
+	let rtc = {
+		client: null,
+		joined: false,
+		published: false,
+		local: null,
+		remote: [],
+		params: {}
+	}
+
+	let options = {
+		appId: "a3e7e983cf0848d08e0b6b97863c344b",
+		channel: null,
+		uid: null,
+		token: null,
+	}
+
+	useEffect(() => {
+		console.log(props.location.state);
+		if(props.location.state === undefined) {
+			setRedirect(true);
+			return;
+		}
+
+		setParams(props.location.state);
+		options.channel = props.location.state.room;
+	}, [props.location.state]);
+
+	useEffect(() => {
+		rtc.client = AgoraRTC.createClient({codec: "h264", mode: "rtc"});
+
+		rtc.client.init(options.appId, () => {
+			console.log("init success");
+		}, (error) => {
+			console.log(error);
+		})
+	}, [])
+
+	if(redirect) {
+		return <Redirect to="/" />
+	}
 	return (
 		<h1>HI</h1>
 	)
--- src/pages/Join.js
+++ src/pages/Join.js
@@ -1,31 +1,26 @@
 import React, { useState } from "react";
-import { Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Join() {
 	const [room, setRoom] = useState("");
-	const [started, setStarted] = useState(false);
 
 	const handleRoomChange = (event) => {
 		setRoom(event.target.value);
 	}
 	
-	if(started) {
-		return (
-			<Redirect push to={{
-				pathname: "/chat",
+	return (
+		<div className="App">
+			<input placeholder="Room" onChange={handleRoomChange} value={room}></input>
+			<Link to={{
+				pathname: '/chat',
 				state: {
 					params: {
 						room
 					}
 				}
-			}} />
-		)
-	}
-
-	return (
-		<div className="App">
-			<input placeholder="Room" onChange={handleRoomChange} value={room}></input>
-			<button onClick={() => setStarted(true)}>Join</button>
+			}}>
+				<button>Join</button>
+			</Link>
 		</div>
 	)
 }
