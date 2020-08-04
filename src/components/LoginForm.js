7a83080d4eb0c1ac0f44dd5eab7f1e1ffbdd5678
Sarthak <sarthakbh321@gmail.com>
2020-08-04 23:40:28 +0530

    form styled

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -9,23 +9,31 @@ function LoginForm(props) {
 		<div className="form-container">
 			<div className="form-div">
 				<Typography variant="h4" className="form-head">Join Meeting</Typography>
-				<TextField variant="outlined" label="Room" onChange={props.roomChange} value={props.room}></TextField>
-
-				<Button
-					className="join-btn"
-					variant="contained"
-					component={Link}
-					to={{
-						pathname: '/chat',
-						state: {
-							params: {
-								room: props.room
+				<Typography variant="p" className="form-subhead">CodeChef-VIT RTC is a video calling platform, made for project reviews.</Typography>
+				<div className="form">
+					<TextField 
+						className="join-input"
+						variant="outlined" 
+						label="Room" 
+						onChange={props.roomChange} 
+						value={props.room}></TextField>
+					<Button
+						className="join-btn"
+						variant="contained"
+						component={Link}
+						to={{
+							pathname: '/chat',
+							state: {
+								params: {
+									room: props.room
+								}
 							}
-						}
-					}}
-				>
-					Join
-				</Button>
+						}}
+					>
+						Join
+					</Button>
+				</div>
+				<Typography variant="p" className="form-footer">Made with love, by CodeChef-VIT.</Typography>
 			</div>
 		</div>
 	)
